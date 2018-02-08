2f9bad1ce17da74796bd9196885cbece5defafa9
Ronaldo3rw <rdias3rw@gmail.com>
2018-02-07 23:05:34 -0200

    Work maps
    Trabalhando com mapas

--- Trabalhando com mapas/ViewController.swift
+++ Trabalhando com mapas/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    
+    // Configura o gerenciador de local - location manager
+    var gerenciadorLocal = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Essa classe (contexto) será responsável por gerenciar o nosso gerenciador local
+        gerenciadorLocal.delegate = self
+        // define a melhor localização possível
+        gerenciadorLocal.desiredAccuracy = kCLLocationAccuracyBest
+        // Pedir autorização ao usuário para utilizar a localização
+        gerenciadorLocal.requestWhenInUseAuthorization()
+        // Atualizando a localização d usuário
+        gerenciadorLocal.startUpdatingLocation()
+        
+        
+        
+        
+        /*
+        // Passo 4) Configurando a latitude e longitude do ponto inicial do mapa.
+        let latitude: CLLocationDegrees = -23.533834
+        let longitude: CLLocationDegrees = -46.680305
+        
+        // Passo 6) Configura o Delta -> Quanto menor o valor mais ele aproxima o zoom
+        let deltaLatitude: CLLocationDegrees = 0.01
+        let deltaLongitude: CLLocationDegrees = 0.01
+        
+        // Passo 3) Configurando a localização e a área de visualização
+        // Recebe latitude e longitude
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        // Passo 5) Configurar área de visualização
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongitude)
+        
+        // Passo 2) Configurando uma Região com localização e área de visualização
+        let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao, areaVisualizacao)
+        
+        // Passo 1) Configurando a região
+        mapa.setRegion(regiao, animated: true)
+        
+        // Incluindo uma anotação (Pin)
+        let anotacao = MKPointAnnotation()
+        
+        // Configurando a anotação
+        anotacao.coordinate = localizacao
+        anotacao.title = "Minha Casa"
+        anotacao.subtitle = "Nada como nosso lar!"
+        
+        // Adiciona anotação no Mapa
+        mapa.addAnnotation(anotacao)
+         */
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
