7b4ed48cd7851ac635ca16a54a4147b84396c15f
Ronaldo3rw <rdias3rw@gmail.com>
2018-02-13 18:55:07 -0200

    Aula 13/02/2018

--- Trabalhando com mapas/ViewController.swift
+++ Trabalhando com mapas/ViewController.swift
@@ -28,42 +28,37 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         // Atualizando a localização d usuário
         gerenciadorLocal.startUpdatingLocation()
         
-        
-        
-        
         /*
-        // Passo 4) Configurando a latitude e longitude do ponto inicial do mapa.
-        let latitude: CLLocationDegrees = -23.533834
+        let latitude: CLLocationDegrees = -23.533834 // Passo 4) Configurando a latitude e longitude do ponto inicial do mapa.
         let longitude: CLLocationDegrees = -46.680305
-        
-        // Passo 6) Configura o Delta -> Quanto menor o valor mais ele aproxima o zoom
-        let deltaLatitude: CLLocationDegrees = 0.01
+        let deltaLatitude: CLLocationDegrees = 0.01 // Passo 6) Configura o Delta -> Quanto menor o valor mais ele aproxima o zoom
         let deltaLongitude: CLLocationDegrees = 0.01
-        
-        // Passo 3) Configurando a localização e a área de visualização
-        // Recebe latitude e longitude
-        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-        
-        // Passo 5) Configurar área de visualização
-        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongitude)
-        
-        // Passo 2) Configurando uma Região com localização e área de visualização
-        let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao, areaVisualizacao)
-        
-        // Passo 1) Configurando a região
-        mapa.setRegion(regiao, animated: true)
-        
-        // Incluindo uma anotação (Pin)
-        let anotacao = MKPointAnnotation()
-        
-        // Configurando a anotação
-        anotacao.coordinate = localizacao
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude) // Passo 3) Configurando a localização e a área de visualização - Recebe latitude e longitude
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongitude) // Passo 5) Configurar área de visualização
+        let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao, areaVisualizacao)  // Passo 2) Configurando uma Região com localização e área de visualização
+        mapa.setRegion(regiao, animated: true) // Passo 1) Configurando a região
+        let anotacao = MKPointAnnotation() // Incluindo uma anotação (Pin)
+        anotacao.coordinate = localizacao // Configurando a anotação
         anotacao.title = "Minha Casa"
         anotacao.subtitle = "Nada como nosso lar!"
+        mapa.addAnnotation(anotacao) // Adiciona anotação no Mapa
+        */
         
-        // Adiciona anotação no Mapa
-        mapa.addAnnotation(anotacao)
-         */
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let localizacaoUsuario: CLLocation = locations.last!
+        
+        // Monta exibição do mapa
+        let latitude: CLLocationDegrees = localizacaoUsuario.coordinate.latitude
+        let longitude: CLLocationDegrees = localizacaoUsuario.coordinate.longitude
+        let deltaLatitude: CLLocationDegrees = 0.01 // Passo 6) Configura o Delta -> Quanto menor o valor mais ele aproxima o zoom
+        let deltaLongitude: CLLocationDegrees = 0.01
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude) // Passo 3) Configurando a localização e a área de visualização - Recebe latitude e longitude
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongitude) // Passo 5) Configurar área de visualização
+        let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao, areaVisualizacao)  // Passo 2) Configurando uma Região com localização e área de visualização
+        mapa.setRegion(regiao, animated: true) // Passo 1) Configurando a região
         
         
     }
